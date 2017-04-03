a0607cb10023014eb2678688c30dd7312f8c514e
SkipjackFlounder <mbtahoematthew@gmail.com>
2017-04-03 10:45:02 -0700

    Create smith.c

---
+++ smith.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int getValue(int, int);
+char getSuit(int);
+
+int main()
+{
+    printf("Welcome to SmithJack!\n");
+    srand(time(0));
+    int choice = 1;
+    double points = 0;
+    do
+    {
+        printf("New round!\n");
+        int card1 = rand()%52;
+        int card2 = rand()%52;
+        int card3 = rand()%52;
+        int card4 = rand()%52;
+        if(getValue(card1, 0) + getValue(card2, 0) == 21)
+            if(getValue(card3, 0) + getValue(card4, 0) == 21)
+            {
+                printf("It's a knock\n");
+                continue;
+            }else
+            {
+                points -= 1.0;
+                continue;
+            }
+        if(getValue(card3, 0) + getValue(card4, 0) == 21)
+        {
+            points += 1.5;
+            printf("You got SmithJack!\n");
+            continue;
+        }
+        printf("The dealer's cards: ");
+        printf("Unknown, %d of %c\n", getValue(card2, 0), getSuit(card2));
+        printf("Your cards: ");
+        printf("%d of %c, %d of %c\n", getValue(card3, 0), getSuit(card3), getValue(card4, 0), getSuit(card4));
+        int total = getValue(card3, 0) + getValue(card4, 0);
+        char c = 's';
+        do
+        {
+            printf("Stay (s) or hit (h)? ");
+            scanf(" %c", &c);
+            if(c == 'h')
+            {
+                int newCard = rand()%52;
+                total += getValue(newCard, total);
+                printf("New card: %d of %c\n", getValue(newCard, total), getSuit(newCard));
+            }
+        }while(c != 's' && total <= 21);
+        if(total > 21)
+            printf("BUST!\n");
+    }while(choice == 1);
+}
+
+
+int getValue(int val, int total)
+{
+    if(val <= 10 * 4)
+        if(val < 4 && total <= 10)
+            return 11;
+        return val/4 + 1;
+    return 10;
+}
+
+
+char getSuit(int val)
+{
+    switch(val%4)
+    {
+        case 0:
+            return 'S';
+        case 1:
+            return 'C';
+        case 2:
+            return 'H';
+        case 3:
+            return 'D';
+    }
+}
