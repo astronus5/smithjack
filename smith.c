c6034086b2142ef25135dcb668ab87e816795e24
astronus5 <danielhatfield1@yahoo.com>
2017-04-05 10:03:08 -0700

    Update smith.c

--- smith.c
+++ smith.c
@@ -183,8 +183,9 @@ void printName(int val)
         case 0:
             printf("Ace");
             break;
-        case 1: case 2: case 3: case 4: case 5:
-        case 6: case 7: case 8: case 9:
+        case 1: case 2: case 3:
+        case 4: case 5: case 6: 
+        case 7: case 8: case 9:
             printf("%d", val/4 + 1);
             break;
         case 10:
